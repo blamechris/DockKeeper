d34e27bc03798fdc4755df1af2577dcd7a123fcb
Christopher <46629049+blamechris@users.noreply.github.com>
2026-07-27 18:41:12 -0700

    Cask: v0.9.1 (#18)
    sha256 from Scripts/notarize.sh's final line (the stapled DMG), per the
    rule v0.9.0 got wrong. Verified against the published asset before
    committing.

--- Casks/dockkeeper.rb
+++ Casks/dockkeeper.rb
@@ -5,8 +5,8 @@
 # package-dmg.sh's hash is pre-staple and will not match what users download.
 # Submit to homebrew/cask central once notability requirements are met.
 cask "dockkeeper" do
-  version "0.9.0"
-  sha256 "2639f68329939b5606d1f5b5799e9017f8e7c4d3eaca7ed3f6a3c6b3536c6137"
+  version "0.9.1"
+  sha256 "ed7bad6bf14d4a7c29b437728d99e5ddde84300327dc2ab8cf46696604cc34d2"
 
   url "https://github.com/blamechris/DockKeeper/releases/download/v#{version}/DockKeeper-#{version}.dmg"
   name "DockKeeper"
